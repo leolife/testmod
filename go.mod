5aa8752bc895fdd4558f59dfec028dc57e38be2a
tomas <hydrogeek@outlook.com>
2018-11-04 22:56:46 +0800

    First commit

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/leolife/testmod
---
+++ test.go
@@ -0,0 +1,10 @@
+package testmod
+
+import (
+	"fmt"
+)
+
+// Hi returns Hi string
+func Hi(name string) string {
+	return fmt.Sprintf("Hi, %s", name)
+}
